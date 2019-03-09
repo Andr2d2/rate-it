3cdc3b54538f150c34a015620577646e4544b2ab
Andre Santos <andre.santos@wooza.com.br>
2019-03-09 14:11:17 -0300

    fix (lib): classe para componete clicavel e inicialização de valor

--- projects/rate-it/src/lib/rate-it.component.ts
+++ projects/rate-it/src/lib/rate-it.component.ts
@@ -1,7 +1,6 @@
 import {
   Component,
   OnInit,
-  SimpleChanges,
   EventEmitter,
   Input,
   Output
@@ -10,15 +9,16 @@ import {
 @Component({
   selector: 'ng-rate-it',
   template: `
-    <div class="rate {{ customClass}}">
-      <span class="item" *ngFor="let unit of rangeAsArray">
+    <div class="rate">
+      <span class="item" *ngFor="let unit of rangeAsArray" [ngClass]="{ '-clickable': !isLocked }">
         <i *ngIf="unit <= newRate" class="icon {{ rateIconClass }}" [ngClass]="{ 'rate-icon' : !rateIconClass }" (click)="setRate(unit)"></i>
         <i *ngIf="unit > newRate" class="icon {{ placeholderIconClass }}" [ngClass]="{ 'placeholder-icon' : !placeholderIconClass }" (click)="setRate(unit)"></i>
       </span>
     </div>
   `,
   styles: [
-    '.item { cursor: pointer; margin: 0 2px; font-size: 30px; }',
+    '.item { margin: 0 2px; font-size: 30px; }',
+    '.item.-clickable { cursor: pointer; }',
     '.icon { font-style: normal; }',
     '.icon.rate-icon:before { content: \'\\02605\'; }',
     '.icon.placeholder-icon:before { content: \'\\02606\'; }'
@@ -52,10 +52,7 @@ export class RateItComponent implements OnInit {
 
   ngOnInit() {
     this.newRate = this.initialRate;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.rangeAsArray = this._buildNumberArray(changes.range.currentValue);
+    this.rangeAsArray = this._buildNumberArray(this.range);
   }
 
   setRate(rate: number) {
