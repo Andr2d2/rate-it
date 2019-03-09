2d8cdcc0646ce7e2310d0e9c9537c2b4c2406481
Andre Santos <andre.santos@wooza.com.br>
2019-03-09 02:10:51 -0300

    feat (app): instanciando lib e configurando-a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rate-it-app';
+  initialValue = 0;
+  rateValue = 0;
+  lock = false;
+
+  changeRate(event: { newRate: number; }) {
+    this.rateValue = event.newRate;
+  }
+
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RateItModule } from 'rate-it';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RateItModule
   ],
   providers: [],
   bootstrap: [AppComponent]
