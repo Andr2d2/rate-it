ba20893d074ae12307d19e09ef08ed7190aca936
Andre Santos <andre.santos@wooza.com.br>
2019-03-09 02:06:55 -0300

    feat (lib): template, css e comportamento

--- projects/rate-it/src/lib/rate-it.component.ts
+++ projects/rate-it/src/lib/rate-it.component.ts
@@ -1,19 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  SimpleChanges,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 
 @Component({
-  selector: 'rit-rate-it',
+  selector: 'rate-it',
   template: `
-    <p>
-      rate-it works!
-    </p>
+    <div class="rate {{ customClass}}">
+      <span class="item" *ngFor="let unit of rangeAsArray">
+        <i *ngIf="unit <= newRate" class="icon {{ rateIconClass }}" [ngClass]="{ 'rate-icon' : !rateIconClass }" (click)="setRate(unit)"></i>
+        <i *ngIf="unit > newRate" class="icon {{ placeholderIconClass }}" [ngClass]="{ 'placeholder-icon' : !placeholderIconClass }" (click)="setRate(unit)"></i>
+      </span>
+    </div>
   `,
-  styles: []
+  styles: [
+    '.item { cursor: pointer; margin: 0 2px; font-size: 30px; }',
+    '.icon { font-style: normal; }',
+    '.icon.rate-icon:before { content: \'\\02605\'; }',
+    '.icon.placeholder-icon:before { content: \'\\02606\'; }'
+  ]
 })
 export class RateItComponent implements OnInit {
 
+  @Input() customClass: string;
+  @Input() initialRate = 1;
+  @Input() placeholderIconClass: string;
+  @Input() range = 5;
+  @Input() rateIconClass: string;
+  @Input() isLocked = false;
+
+  @Output() onChangeRate = new EventEmitter();
+
+  newRate: number;
+  rangeAsArray: number[];
+
   constructor() { }
 
+  private _buildNumberArray(value: number): number[] {
+    const array = [];
+
+    for (let i = 1, length = value; i <= length; i++) {
+      array.push(i);
+    }
+
+    return array;
+  }
+
   ngOnInit() {
+    this.newRate = this.initialRate;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.rangeAsArray = this._buildNumberArray(changes.range.currentValue);
+  }
+
+  setRate(rate: number) {
+    if (!this.isLocked) {
+      this.newRate = rate;
+      this.onChangeRate.emit({ newRate: rate });
+    }
   }
 
 }
